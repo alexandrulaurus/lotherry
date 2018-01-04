2b2105ae51355090c63eb5dcaf5e129251baada6
Alexandru Laurus <alexandru.laurus@gmail.com>
2018-01-04 09:49:39 +0200

    Hmm

---
+++ lotherry.py
@@ -0,0 +1,80 @@
+import logging
+import os
+import time
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoAlertPresentException
+
+logging.basicConfig(level=logging.INFO,
+        format='%(asctime)s [%(levelname)s] (%(threadName)-10s) %(message)s',
+        )
+
+
+MAX_ATTEMPTS = os.getenv('MAX_ATTEMPTS', 50)
+OUTPUT_FILE = os.getenv('OUTPUT_FILE', 'winning_numbers')
+
+def acceptAlert(driver):
+    try:
+        alert = driver.switch_to.alert
+        alert.accept()
+        logging.info("Alert accepted")
+    except NoAlertPresentException:
+        logging.info("No alert. Parsing...")
+
+
+def main():
+    logging.info("Configuring options...")
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1200x600')
+    logging.info("Starting webdriver...")
+    driver = webdriver.Chrome(chrome_options=options)
+
+    output = open('file_{}_{}'.format(OUTPUT_FILE, int(time.time())), 'w')
+    output.write("      key     |       address     |       balance     \n")
+    output.flush()
+
+    while True:
+        logging.info("Fetching page...")
+        driver.get("http://www.ethersecret.com/random")
+        acceptAlert(driver)
+        rows = driver.find_elements(By.TAG_NAME, "tr")
+        acceptAlert(driver)
+        for rowIdx, row in enumerate(rows):
+            if (rowIdx == 0):
+                continue
+            columns = row.find_elements(By.TAG_NAME, "td")
+            acceptAlert(driver)
+            for attempt in range(1, MAX_ATTEMPTS):
+                logging.info("Attempt %d of %d", attempt, MAX_ATTEMPTS)
+                rowInfo = {}
+                for idx, val in enumerate(columns):
+                    if (idx == 0):
+                        rowInfo['key'] = val.text
+                    if (idx == 1):
+                        rowInfo['address'] = val.text
+                    if (idx == 2):
+                        rowInfo['balance'] = val.text
+                try:
+                    balance = rowInfo['balance']
+                    if (balance == ""):
+                        time.sleep(0.2)
+                        continue
+                    logging.info("%s | %s | %s", rowInfo['key'], rowInfo['address'], rowInfo['balance'])
+                    if (balance != "0"):
+                        logging.info("Bingo: %s | %s | %s", rowInfo['key'], rowInfo['address'], rowInfo['balance'])
+                        output.write(rowInfo['key'] + " " + rowInfo['address']
+                                + " " + rowInfo['balance'] + "\n")
+                        output.flush()
+                    break
+                except KeyError:
+                    time.sleep(1)
+                    continue
+                logging.info("%s | %s | %s", rowInfo['key'], rowInfo['address'], rowInfo['balance'])
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
